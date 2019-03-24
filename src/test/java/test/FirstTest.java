4c2b3c17dc787afc96b230172f3abdf42b439396
meowmik <tosegida@edu.hse.ru>
2019-03-24 19:33:54 +0300

    SecondTest was created

--- src/test/java/test/FirstTest.java
+++ src/test/java/test/FirstTest.java
@@ -15,16 +15,30 @@ import org.openqa.selenium.*;
 			driver.findElement(By.name("email")).click();
 			driver.findElement(By.name("phone")).click();
 			driver.findElement(By.name("socialLink0")).click();
-			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Перетащите файлы сюда'])[1]/following::p[3]")).click();
-			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Перетащите файлы сюда'])[1]/following::span[2]")).click();
-			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Заполните анкету'])[1]/following::div[2]")).click();
-			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Заполните анкету'])[1]/following::div[2]")).click();
-			// ERROR: Caught exception [ERROR: Unsupported command [doubleClick | xpath=(.//*[normalize-space(text()) and normalize-space(.)='Заполните анкету'])[1]/following::div[2] | ]]
-			driver.findElement(By.name("birthday")).click();
-			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Дата рождения'])[1]/following::div[3]")).click();
-			driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Дата рождения'])[1]/following::div[3]")).click();
+			driver.findElement(By.cssSelector("svg.ui-icon-checkbox.ui-checkbox__icon")).click();
 		}
 
+		@Test
+		public void testSecond(){
+			driver.get(baseUrl);
+			driver.findElement(By.name("name")).click();
+			driver.findElement(By.name("name")).clear();
+			driver.findElement(By.name("name")).sendKeys("kugf");
+			driver.findElement(By.name("birthday")).click();
+			driver.findElement(By.name("birthday")).clear();
+			driver.findElement(By.name("birthday")).sendKeys("34.");
+			driver.findElement(By.name("city")).click();
+			driver.findElement(By.name("city")).clear();
+			driver.findElement(By.name("city")).sendKeys("дкпло");
+			driver.findElement(By.name("email")).click();
+			driver.findElement(By.name("email")).clear();
+			driver.findElement(By.name("email")).sendKeys("lknlk");
+			driver.findElement(By.name("phone")).click();
+			driver.findElement(By.name("phone")).clear();
+			driver.findElement(By.name("phone")).sendKeys("+7(234)");
+			driver.findElement(By.name("socialLink0")).click();
+			driver.findElement(By.cssSelector("svg.ui-icon-checkbox.ui-checkbox__icon")).click();
+		}
 	}
 
 
